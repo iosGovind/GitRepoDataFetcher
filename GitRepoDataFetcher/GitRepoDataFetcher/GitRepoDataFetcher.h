118f93cd01ceebb0bbf190e26bda7b906fd0a565
Govind Solanki <govind.solanki@appinventiv.com>
2022-08-19 12:01:53 +0530

    Adding framework

---
+++ GitRepoDataFetcher/GitRepoDataFetcher/GitRepoDataFetcher.h
@@ -0,0 +1,18 @@
+//
+//  GitRepoDataFetcher.h
+//  GitRepoDataFetcher
+//
+//  Created by Govind Solanki on 2022-08-19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitRepoDataFetcher.
+FOUNDATION_EXPORT double GitRepoDataFetcherVersionNumber;
+
+//! Project version string for GitRepoDataFetcher.
+FOUNDATION_EXPORT const unsigned char GitRepoDataFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitRepoDataFetcher/PublicHeader.h>
+
+
